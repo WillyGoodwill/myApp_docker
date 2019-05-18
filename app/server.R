1de8253e6c0fc44047415e68a78c40d01b80a708
Olga Lazunina <olga.lazunina@gmail.com>
2019-05-18 23:39:37 +0300

    #1 task1 first commit

--- app/server.R
+++ app/server.R
@@ -10,17 +10,30 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+# shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
+#   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#     # generate bins based on input$bins from ui.R
+#     x    <- faithful[, 2] 
+#     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+#     # draw the histogram with the specified number of bins
+#     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+#   })
+
+  # runApp()
+  
+# })
+
+server = {
+  shinyServer(function(input, output, session) {
+    
+    # MyData <- read.csv(file="ldb_data.csv", header=TRUE, sep=",")
+    
+    # callModule(chartTableBox, id = "ATC_Topline", data = MyData)
+    callModule(dropDown, id = "dropdown")
     
   })
-  
-})
+}
--- app/ui.R
+++ app/ui.R
@@ -8,26 +8,16 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
 
-# Define server logic required to draw a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("New title UPDATED"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+shinyUI(fluidPage(dashboardPage(
+  dashboardHeader(
+     title="Modelling Tool", 
+     titleWidth=290
+  ),
+  sidebar,
+  body
+)
+)
+)
