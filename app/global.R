87fceccdf9e5122d4d83b4542b07cc6f6883ed44
Olga Lazunina <olga.lazunina@gmail.com>
2019-05-19 20:30:35 +0300

    - creating sidebar menu - Data

--- app/global.R
+++ app/global.R
@@ -2,66 +2,10 @@ library(shiny)
 library(shinydashboard)
 library(shinyWidgets)
 
-dropDownUI <- function(id, div_width = "col-xs-12 col-md-8") {
-  
-  ns <- NS(id)
-  
-  div(column(3, uiOutput(ns("class_level"))),
-      column(
-        width = 3,
-        # selectInput(
-        #   inputId = ns("selected_product"),
-        #   label = h4("Product Family"),
-        #   choices <- c("22","33","44"),
-        #   width = "100%"
-        # )
-        uiOutput(ns("selected_product_ui"))
-      ))
-}
-
-dropDown <- function(input, output, session) {
-  
-  ns <- session$ns
-  
-  output$class_level <- renderUI({
-    selectInput(
-      ns("selected_class"),
-      label = h4("Classification Level"),
-      choices = list(
-        "apple " = "apple",
-        "orange " = "orange"),
-      selected = "orange"
-    )})
-  
-  output$selected_product_ui <- renderUI({
-    req(input$selected_class)
-    Sys.sleep(0.2)
-    ns <- session$ns
-    
-    if (input$selected_class == "apple") {
-      my_choices <- c("foo","zoo","boo")
-    } else if (input$selected_class == "orange") {
-      my_choices <- c("22","33","44")
-    } else {
-      my_choices <- c("aa","bb","cc")
-    }
-    
-    selectInput(inputId = ns("selected_product"),
-                      label = h4("Product Family"),
-                      choices = my_choices)
-  })
-  
-}
+source("SideBarMenu.R")
 
 
-
-sidebar <- dashboardSidebar(sidebarMenu(
-  menuItem("aaa",tabName = "aaa"),
-  menuItem("bbb", tabName = "bbb"),
-  menuItem("ccc", tabName = "ccc")
-))
-
-body <- dashboardBody(tabItems(
+dashboardBody1 <- dashboardBody(tabItems(
     tabItem(tabName = "aaa",
             fluidRow(dropDownUI(id = "dropdown"))
             # fluidRow(chartTableBoxUI(id = "ATC_Topline"))) # render the tabBox inside a fluidRow
--- app/server.R
+++ app/server.R
@@ -1,39 +1,12 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define server logic required to draw a histogram
-# shinyServer(function(input, output) {
-   
-#   output$distPlot <- renderPlot({
-    
-#     # generate bins based on input$bins from ui.R
-#     x    <- faithful[, 2] 
-#     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-#     # draw the histogram with the specified number of bins
-#     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-#   })
-
-  # runApp()
-  
-# })
-
 server = {
-  shinyServer(function(input, output, session) {
-    
-    # MyData <- read.csv(file="ldb_data.csv", header=TRUE, sep=",")
-    
-    # callModule(chartTableBox, id = "ATC_Topline", data = MyData)
+  shinyServer(function(input, output, session) {    
     callModule(dropDown, id = "dropdown")
-    
+
+    output$menu<-renderMenu({
+      sidebarMenuFunc()
+    })
   })
 }
